72982ed5db58492904b617e535e8896b3ddec402
preeves86 <preeves86@yahoo.com>
2013-03-20 21:35:12 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omproject::Application.config.secret_token = '14881ba9b0e3ebcc27ef72cfbf543b63e2c60758843c3a219b12046b8070130c9a88999a3128c68165ea40d110d8160f458ed8be03c87c172818bf4876478d8d'
